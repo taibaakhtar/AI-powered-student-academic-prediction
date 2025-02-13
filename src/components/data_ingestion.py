4f73b9790eebcba6584120e898407cd6b3012bdc
unknown <taibaakhtar42@gmail.com>
2025-02-13 13:52:08 +0530

    Data transformation donw

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+
 
 @dataclass  # decorator, used to create a data class
 class DataIngestionConfig:
@@ -39,6 +41,10 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
             
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import dill
+from src.exception import CustomException
+
+def save_object(obj, file_path):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file:
+            dill.dump(obj, file)
+
+    except Exception as e:
+        raise CustomException(e, sys)
